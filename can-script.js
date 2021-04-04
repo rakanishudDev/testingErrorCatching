637ec45bb2f84758a8e5ae8a0961e42ce1f787c9
RakanishuDev <80227542+rakanishudDev@users.noreply.github.com>
2021-04-04 18:24:22 +0300

    Update can-script.js

--- can-script.js
+++ can-script.js
@@ -4,14 +4,18 @@ var products;
 // use fetch to retrieve it, and report any errors that occur in the fetch operation
 // once the products have been successfully loaded and formatted as a JSON object
 // using response.json(), run the initialize() function
-let request = new XMLHttpRequest();
-request.open('GET', 'productds.json');
-request.responseType = 'json';
-request.onload = function() {
-  products = request.response;
-  initialize();
-};
-request.send();
+
+function fetchingOldStyle() {
+  let request = new XMLHttpRequest();
+  request.open('GET', 'productds.json');
+  request.responseType = 'json';
+  request.onload = function() {
+    products = request.response;
+    initialize();
+  };
+  request.send();
+}
+fetchingOldStyle();
 // sets up the app logic, declares required variables, contains all the other functions
 function initialize() {
   // grab the UI elements that we need to manipulate
